ef491f934605ffff36b48ab314623b323bbaf0ce
Jtachan <jaimenicolas.gonzalezg@gmail.com>
2024-08-13 20:04:47 +0200

    Changes in previous codes

---
+++ 2-farenh2celsius.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/* Here mathematic operations are performed over a value given by the user. */
+
+int main() {
+    float temp_f;
+    float temp_c;
+
+    // Without 'endl' the user writes right next to the sentence:
+    cout << "Enter a temperature in fahrenheit: ";
+    cin >> temp_f;
+    
+    temp_c = (temp_f - 32) * 5.0f / 9.0f;
+    cout << "Your temperature in celsius is " << temp_c << endl;
+
+    return 0;
+}
