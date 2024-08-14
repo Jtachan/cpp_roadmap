a702bed42cadf8b2153a832ce6e312efe911e713
Jtachan <jaimenicolas.gonzalezg@gmail.com>
2024-08-14 18:19:52 +0200

    Arrays

---
+++ 5-sum_of_array.cpp
@@ -0,0 +1,39 @@
+/* Program to sum the numbers in an array.
+ * 
+ * This exercise practises:
+ *    - Definition & initialization of an array.
+ *    - for-loops
+ *    - Understanding of 'sizeof()'
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int result = 0;
+    // Definition of an array with 10 ints
+    int numbers[10];
+
+    /* Dynamic find of how long the array is:
+     * 'sizeof()' returns the number of bytes of an element.
+     * Because the array has all elements of the same type, each
+     * element has the same size.
+     * Thus, the total number of elements is defined by the
+     * total size in bytes of the array over the size of one element.
+    */
+    int arr_size = sizeof(numbers) / sizeof(numbers[0]);
+
+    // Populate the array with the user input:
+    cout << "Provide 10 numbers to be added:" << endl;
+    for (int i = 0; i < arr_size; i++) {
+        cin >> numbers[i];
+    }
+
+    // Next syntaxis is valid only from C++11
+    for (int number : numbers) {
+        // Sum all numbers
+        result += number;
+    }
+
+    cout << "The sum of the numbers is " << result << endl;
+}
