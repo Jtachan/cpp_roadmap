ec6db865a2a91ba1660b92753c0d03aaba7ebe1e
Jtachan <jaimenicolas.gonzalezg@gmail.com>
2024-08-24 13:14:48 +0200

    I/O process

---
+++ 8-file_process.cpp
@@ -0,0 +1,67 @@
+/* This program reads a file containing numbers and writes a new file
+ * with the result of the sum of all the numbers.
+ *
+ * This exercise practises:
+ *   - Handling I/O objects.
+ */
+
+#include <fstream>
+#include <iostream>
+
+int read_file(std::string file_name) {
+    std::cout << "Reading a file to sum all the numbers within it..."
+              << std::endl;
+
+    // Opening file (for reading only):
+    std::ifstream inFile(file_name);
+
+    if (!inFile) {
+        throw std::runtime_error("Could not open the specified file.");
+    }
+    if (inFile.peek() == std::ifstream::traits_type::eof()) {
+        throw std::runtime_error("The given file is empty.");
+    }
+
+    // Code here:
+    int result = 0;
+    int f_number;
+
+    while (inFile >> f_number) {
+        result += f_number;
+    }
+
+    // Closing file:
+    inFile.close();
+
+    return result;
+}
+
+void write_file(int value) {
+    std::cout << "Writing the result into a new file..." << std::endl;
+
+    // Opening file (for writing only):
+    std::ofstream outFile("result.txt");
+
+    if (!outFile) {
+        throw std::runtime_error("Could not open the specified file.");
+    }
+
+    // Writing the data into the file:
+    outFile << value << std::endl;
+
+    // Closing the file:
+    outFile.close();
+}
+
+int main() {
+    try {
+        int result = read_file("numbers.txt");
+        write_file(result);
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << '\n';
+    }
+
+    std::cout << "File 'result.txt' was created with the total sum "
+              << "of the numbers within 'numbers.txt'." << std::endl;
+    return 0;
+}
